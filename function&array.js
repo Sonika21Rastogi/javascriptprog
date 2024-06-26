85e7511d5a1107a87987e9d5ec1bebf7bbea45be
Sonika21Rastogi <Snkrstg@gmail.com>
2024-06-26 12:34:24 +0530

    new commit to changes

---
+++ function&array.js
@@ -0,0 +1,126 @@
+// console.log('hello world');
+
+// X = 4;
+// y = 6;
+// // 10 ; addition
+
+// function sum(x, y) {
+//     total = x + y;
+//     console.log(total);
+// };
+// sum(4, 6);
+
+
+// x = 2;
+// y = 4;
+
+// // 8 ; multipliction 
+
+// function multi(x,y) {
+//     multiplic = x * y;
+//     console.log(multiplic);
+// };
+// multi(2,4);
+
+// // ARRAY
+
+// x = [1,2,3,4]
+
+// // 1
+// // 2
+// // 3
+// // 4
+
+// var x = [1,2,3,4];
+// for(var i=0; i<x.length; i++){
+//     console.log(x[i]);
+// };
+
+// array [2,3,4,5,6]; each add one
+
+// x = [1,2,3,4,5];
+// y = 0;
+
+// for(var i=0; i<5; i++){
+//     y= x[i] +',';
+//     console.log(y);
+
+// };
+
+// x = [1,2,3,4,5,6];
+// y = [];
+// z = 0;
+
+// for(var i=0; i<x.length; i++){
+//   z = x[i] + y;
+//   flag++;
+
+//   console.log(z);  
+// };
+
+// arr = [1,2,3,4,5,6];
+
+// console.log(arr);
+
+// x = [1,2,3,4,5];
+
+// for(var i=0; i<x.length; i++){
+//     z = x[i] ;
+//     console.log(z);
+// };
+
+// x = [1,2,3,4,5,6];
+// y = [];
+// flag = 0;
+
+// for(var i = 0; i<x.length; i++){
+//     y = x[i] + flag;
+//     flag++;
+//     console.log(y);
+// };
+
+// even and odd
+
+// for(var i=1; i<=20; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+    
+// };
+
+// for(var i=1; i<=30; i++){
+//     if(i % 3 == 0){
+//         console.log(i);
+//     }
+    
+// };
+
+// functions:-
+
+function fun(x,y){
+    sum = x - y;
+    console.log(sum);
+};
+fun(20,5);
+
+//  for....in // Arrays
+
+x = ['soni', 'moni','mohit', 'ladoo'];
+
+for(let key in x){
+    console.log(key,x[key]);
+ 
+};
+
+let animal ={
+    name:"cow",
+    leg:4
+};
+
+for(let key in animal){
+    console.log(key,animal[key])
+};
+
+
+
+
